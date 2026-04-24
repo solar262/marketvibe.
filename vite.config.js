d35069683d56d43a0f9ff97a30c56d72d7a814cb
solar262 <solardynamics592@gmail.com>
2026-04-24 12:38:18 +0200

    fix: polyfill process.env in vite for client modules

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  define: {
+    'process.env': {}
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
